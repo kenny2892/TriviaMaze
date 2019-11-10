0811adbe8124afb5e3325c1396674612e2d9bb18
Hunter Matthew Rich <xxrichmanxx@gmail.com>
2019-11-09 22:42:56 -0800

    Maze Builder
    Generic Maze builder.

--- Trivia Maze/src/application/Maze.java
+++ Trivia Maze/src/application/Maze.java
@@ -13,6 +13,8 @@ public class Maze extends Application
 	private int playerX;
 	private int playerY;
 	
+	private static Room[][] gameMaze;
+	
 	@Override
 	public void start(Stage primaryStage)
 	{
@@ -45,6 +47,178 @@ public class Maze extends Application
 
 	public static void main(String[] args)
 	{
+		final int mazeRows = 5;
+		final int mazeColumns = 5;
+		mazeBuilder(mazeRows, mazeColumns);
+		
 		launch(args);
+	}
+	
+	private static void mazeBuilder(final int mazeRows, final int mazeColumns)
+	{
+		gameMaze = new Room[mazeRows][mazeColumns];
+		
+		setUpInsideMaze(mazeRows, mazeColumns);
+		setUpMazeBorder(mazeRows, mazeColumns);
+		setUpMazeCorners(mazeRows, mazeColumns);
+	}
+	
+	private static void setUpInsideMaze(final int mazeRows, final int mazeColumns)
+	{
+		boolean doorIsLocked = false;
+		Question defaultQuestion = new NullQuestion();
+		Door genericDoor = new Door(doorIsLocked, defaultQuestion);
+		
+		for(int rowIndex = 1; rowIndex < mazeRows - 1; rowIndex++)
+		{
+			for(int columnIndex = 1; columnIndex < mazeColumns - 1; columnIndex++)
+			{
+				Door northDoor = genericDoor;
+				Door southDoor = genericDoor;
+				Door eastDoor = genericDoor;
+				Door westDoor = genericDoor;
+				
+				gameMaze[rowIndex][columnIndex] = new Room
+						(
+						 northDoor,
+						 southDoor,
+						 eastDoor,
+						 westDoor
+						 );
+			}
+		}
+	}
+	
+	private static void setUpMazeBorder(final int mazeRows, final int mazeColumns)
+	{
+		boolean doorIsLocked = false;
+		Question defaultQuestion = new NullQuestion();
+		Door genericDoor = new Door(doorIsLocked, defaultQuestion);
+		
+		for(int columnIndex = 0; columnIndex < mazeColumns - 1; columnIndex++)
+		{
+			// Top
+			Door northDoor = new NullDoor();
+			Door southDoor = genericDoor;
+			Door eastDoor = genericDoor;
+			Door westDoor = genericDoor;
+			
+			gameMaze[0][columnIndex] = new Room
+					(
+					northDoor,
+					southDoor,
+					eastDoor,
+					westDoor
+					);
+			
+			// Bottom
+			northDoor = genericDoor;
+			southDoor = new NullDoor();
+			eastDoor = genericDoor;
+			westDoor = genericDoor;
+			
+			gameMaze[mazeRows][columnIndex] = new Room
+					(
+					northDoor,
+					southDoor,
+					eastDoor,
+					westDoor
+					);
+		}
+		
+		for(int rowIndex = 0; rowIndex < mazeColumns - 1; rowIndex++)
+		{
+			// Left
+			Door northDoor = genericDoor;
+			Door southDoor = genericDoor;
+			Door eastDoor = genericDoor;
+			Door westDoor = new NullDoor();
+			
+			gameMaze[rowIndex][0] = new Room
+					(
+					northDoor,
+					southDoor,
+					eastDoor,
+					westDoor
+					);
+			
+			// Right
+			northDoor = genericDoor;
+			southDoor = new NullDoor();
+			eastDoor = genericDoor;
+			westDoor = genericDoor;
+			
+			gameMaze[rowIndex][mazeColumns] = new Room
+					(
+					northDoor,
+					southDoor,
+					eastDoor,
+					westDoor
+					);
+		}
+		
+	}
+	
+	private static void setUpMazeCorners(final int mazeRows, final int mazeColumns)
+	{
+		boolean doorIsLocked = false;
+		Question defaultQuestion = new NullQuestion();
+		Door genericDoor = new Door(doorIsLocked, defaultQuestion);
+		
+		// Top Left
+		Door northDoor = new NullDoor();
+		Door southDoor = genericDoor;
+		Door eastDoor = genericDoor;
+		Door westDoor = new NullDoor();
+		
+		gameMaze[0][0] = new Room
+				(
+				northDoor,
+				southDoor,
+				eastDoor,
+				westDoor
+				);
+		
+		// Top Right
+		northDoor = new NullDoor();
+		southDoor = genericDoor;
+		eastDoor = new NullDoor();
+		westDoor = genericDoor;
+		
+		gameMaze[0][mazeColumns] = new Room
+				(
+				northDoor,
+				southDoor,
+				eastDoor,
+				westDoor
+				);
+		
+		// Bottom Right
+		northDoor = genericDoor;
+		southDoor = new NullDoor();
+		eastDoor = new NullDoor();
+		westDoor = genericDoor;
+		
+		gameMaze[mazeRows][mazeColumns] = new Room
+				(
+				northDoor,
+				southDoor,
+				eastDoor,
+				westDoor
+				);
+		
+		// Bottom Left
+		northDoor = genericDoor;
+		southDoor = new NullDoor();
+		eastDoor = genericDoor;
+		westDoor = new NullDoor();
+		
+		gameMaze[mazeRows][0] = new Room
+				(
+				northDoor,
+				southDoor,
+				eastDoor,
+				westDoor
+				);
 	}
 }
