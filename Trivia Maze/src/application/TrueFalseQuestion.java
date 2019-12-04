08b8a8132f3b0feeed7befa5295a655cd757a87e
Kenny <jessetuan@comcast.net>
2019-12-04 14:28:55 -0800

    Added serialization ids

--- Trivia Maze/src/application/TrueFalseQuestion.java
+++ Trivia Maze/src/application/TrueFalseQuestion.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public class TrueFalseQuestion extends Question
 {
+	private static final long serialVersionUID = 4816361754788452921L;
+
 	public TrueFalseQuestion(String question, ArrayList<String> answers, int correctIndex)
 	{
 		super(QuestionType.TRUE_FALSE, question, answers, correctIndex);
